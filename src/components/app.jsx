28aab3b665cf9a6e53c2dd45c65e0e6400cb1736
chooseofgod <791970698@qq.com>
2020-06-22 14:56:45 +0800

    '搭建'

---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import {Button} from "antd-mobile";
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type='primary'>我的</Button>
+      </div>
+    )
+  }
+}
